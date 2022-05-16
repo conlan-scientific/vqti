296f1a884e3ec5fc23dfb3b9e55fc0cd5d8aae1f
Kevin Tagoe <kevin.tagoe@gordon.edu>
2022-05-16 00:14:38 -0400

    An attempt at a scalable %B and bandwidth indicator.

--- kevsrc/pB.py
+++ kevsrc/pB.py
@@ -8,49 +8,86 @@ import pandas as pd
 import numpy as np
 from typing import List
 
+
 """
-Simple Moving Average
+Bollinger Bands, %B, and Bandwidth
 """
 
-def python_sma_k (values: List[float], n: int = 2, m: int = 2):
+def python_pB (values: List[float], n: int = 2):
 
     values = load_eod('AWU') # loads the dataframe in question
-    close = values.loc[:,'close'] # loads only the values in the close column
+    value_range = values.loc[:,'close'] # loads only the values in a specific column. In this case, "close".
 
-    # simple moving average formula
-    amov = [0] * (n-1) # initializes the moving average formula
-    rolling_total = sum(close[:n]) # adds up numbers associated with the moving average range
-    amov.append(rolling_total/n)
+    #High Level Variables
+    N = 1 #A multiplier for the standard deviation
     
-    for k in range(n, len(close)):  #loops moving average formula through column range
-        rolling_total = rolling_total - close[k-n]
-        rolling_total = rolling_total + close[k]
-        amov.append(rolling_total/n)
+    amov = 0 #Initializes the moving average
+    
+    dev = 0 #Initializes the differences from the moving average
+    devsum = 0 #Initializes the sum of the differences
+
+    var = 0 #Initializes the variance
+    
+    stddev_j = [] #Initializes the standard deviation list
+    
+    amovl = []
+    rolling_total = 0
+    
+    BBu = 0 #The initialization of the Upper Bollinger Band values
+    BBl = 0 #The initialization of the Lower Bollinger Band values
+    pB = [] #Initializes the %B list    
+    bandwidth = []
+
+    #Simple moving average initialization
+    rolling_total = sum(value_range[:n]) #Adds up the initial numbers associated with the moving average range
+    amov = rolling_total/n #Calculates the first moving average
+    amovl.append(amov) #For testing that amov is working correctly. It should update with new values of amov
+    
+    
+    #Standard deviation initialization   
+    for a in range (0, n):
+        dev = value_range[a] - amov #The initial interval-summed differences between the column values and the moving average
+    
+    devsum = 0 + dev
+    
+    var = devsum**2 #The variance of the deviations
+    stddev = math.sqrt(var) #The initial standard deviation 
+    stddev_j.append(N*stddev) #Defines the number of standard deviations under consideration and places each incremented value in a list
+    
+    for k in range (n, len(value_range),n): #Loops the moving average and standard deviation formulas to form a standard deviation list
+        rolling_total = rolling_total - value_range[k-n] + value_range[k] #Subtracts the oldest value off of the moving average total, then adds the newest value onto the moving average total
+        amov = rolling_total/n #Calculates the moving average based on the current rolling total
+        amovl.append(amov) #Appends amovl with the newest value of amov
         
-        if amov[0] == 0:
-            del amov[0] # truncates 0 values off of moving average list
-    
+        dev = value_range[k] - amov #The listed differences between the close column values and the moving average at each interval as defined in the loop
+     
+
+        devsum = 0 + dev
         
-    N = 2 #multiplier for the standard deviation
-    stddev_k = [0] * (n-1) # initializes the standard deviation formula
+        var = devsum**2 #The variance of the differences
+        stddev = math.sqrt(var) #The standard deviation
+        stddev_j.append(N*stddev) #Adds the standard deviation onto the end of the standard deviation list
+
+        BBu = amov + stddev #Adds the moving average and the standard deviation at each increment and places the final value at the end of the Upper Bollinger Band list
+        BBl = amov - stddev #Subtracts the standard deviation from the moving average at each increment and places the final value at the end of the Lower Bollinger Band list 
         
-    for j in range(m, len(close)): #a loop to append values to a standard deviation list
-        dev = (amov[j] - amov[m])**2
-        var = sum(dev)/m
-        stddev = math.sqrt(var)
-        stddev_k.append(N*stddev)
+        pB.append((value_range[k] - BBl)/(BBu - BBl))
     
-        if stddev_k[0] == 0:
-            del stddev_k[0] # truncates 0 values off of standard deviation list
+        bandwidth.append((BBu-BBl)/amov)
+        
+    return amovl
     
-    return stddev_k
+    
 
 
 
 if __name__=="__main__":    
-    
+   
     df = load_eod('AWU')
-    print (df)
+    c = df.loc[:,'close'] 
+    d = c[0:len(c)]
+    print (d)
     
-    amov_result = python_sma_k(df)
+    
+    amov_result = python_pB(df)
     print (amov_result)
