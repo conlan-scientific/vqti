1f2dabdcbc5891a8aad6ebe0f3fc18444390343f
Chris Conlan <chris@conlan.io>
2022-05-16 18:47:38 -0400

    saving

--- src/02_chris_sample_performance_indicators.py
+++ src/02_chris_sample_performance_indicators.py
@@ -18,8 +18,65 @@ if __name__ == '__main__':
 
 	df = load_eod('AWU')
 
+	percent_b = calculate_percent_b(df.close)
+	df = df.assign(percent_b=percent_b)
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+P_t   (Portfolio)
+C_t   (Cash)
+E_t   (Equity)
+
+E_t = P_t + C_t
+P_t = sum of all shares times prices
+
+Measure performance on E_t
+
+-------
+
+Risk-adjusted return
+
+1. Measure return
+Overall return = (E_T / E_0) - 1
+^^^ Annualize it, and you have the CAGR
+
+2. Measure risk
+Standard deviation of return series of the equity curve
+sigma_{r_t}
+r_t = (E_t / E_{t-1}) - 1
+^^^ Annualize it, and you have "Volatility"
+
+3. Divide one by the other
+Sharpe ratio is the CAGR over the volatility.
+
+"""
