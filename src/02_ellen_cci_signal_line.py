d7c8a4041084976a221ceb2e0298d7e8590ca496
Elle <ellenyixinyu@gmail.com>
2022-05-18 15:59:40 -0400

    wip signals function

---
+++ src/02_ellen_cci_signal_line.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 17 17:02:38 2022
+
+@author: ellenyu
+
+To do: 
+    - put rolling function into cci - DONE 
+    - technically, I should keep testing cci 
+    - write a signal converter function 
+        - tehcnically, I should visualize and analyze the results  
+"""
+
+
+def signals_generator (function, myinput, st_dev):
+    
+    # run function on df
+    
+    # calculates the std 
+    
+    # sell signal if > 2 std  above mean 
+    
+    # buy signal if > 2 std below mean 
+    
