53670c045ad7dd6bf1284058a586d76f1fe46a1c
Elle <ellenyixinyu@gmail.com>
2022-05-18 21:08:53 -0400

    very minor changes

--- src/01_ellen_rollingSum_pandas_python_numpy_numba.py
+++ src/01_ellen_rollingSum_pandas_python_numpy_numba.py
@@ -10,6 +10,7 @@ Pending:
     * Program something to handle when users enter window = 0 or window = negative number
     * How do I denote via type hints that it is a pd.Series of floats? 
     * Address the work arounds so, I can extract rolling functions from rollingsum functions
+    * Create a numbas implementation and negative test cases 
 
 @author: ellenyu
 
