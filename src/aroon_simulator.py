b57f759731fe582e97409185ee04d24d23efdf63
Dominic White <dewhite4@gmail.com>
2022-05-23 16:07:41 -0400

    Add Trading Simulator class; refactor tests to separate file

--- src/aroon_simulator.py
+++ src/aroon_simulator.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from typing import List
-import unittest
 
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from aroon_oscillator import aroon_python_deque, aroon_signal_line
 
@@ -13,11 +13,12 @@ class HistoricalData():
         self.data = None
 
     def _read_csv(self, file_path: Path) -> pd.DataFrame:
-        df = pd.read_csv(file_path, usecols=["high", "low"])
+        df = pd.read_csv(file_path, usecols=["date","high", "low", "close"], index_col="date")
         df = df.rename(
             columns={
                 "high": file_path.stem + "_high",
-                "low": file_path.stem + "_low"
+                "low": file_path.stem + "_low",
+                "close": file_path.stem + "_close"
             }
         )
         return df
@@ -41,7 +42,7 @@ class SignalCalculator():
         aroon_oscillator: List = aroon_python_deque(df[f"{stock}_high"].tolist(), df[f"{stock}_low"].tolist())
         aroon_as_series = pd.Series(
             data = aroon_oscillator,
-            name = f"{stock}_aroon",
+            name = f"{stock}",
             index = df.index
         )
         aroon_signal = aroon_signal_line(aroon_as_series)
@@ -62,58 +63,144 @@ class SignalCalculator():
             axis=1
         )
 
-class HistoricalDataTestCase(unittest.TestCase):
+class TradingSimulator():
 
-    def test_read_csv(self):
-        test_file: Path = Path(__file__).parent.parent / "data" / "eod" / "AWU.csv"
-        hd_df: pd.DataFrame = HistoricalData()._read_csv(test_file)
-        self.assertIn("AWU_high", hd_df.columns)
-        self.assertIn("AWU_low", hd_df.columns)
-        self.assertEqual(len(hd_df.columns), 2)
+    def __init__(self, hd, max_assets: int= 20, starting_cash: int = 100000):
+        self.max_assets = max_assets
+        self.current_assets = 0
+        self.cash = starting_cash
+        self.price_df = self._get_price_df(hd)
+        self.portfolio = {}
 
-    def test_load_data(self):
-        test_dir: Path = Path(__file__).parent.parent / "data" / "eod"
-        hd: HistoricalData = HistoricalData()
-        hd.load_data(test_dir)
-        self.assertEqual(len(hd.data.columns), 200)
-        self.assertIn("AWU_high", hd.data.columns)
-        self.assertIn("AWU_low", hd.data.columns)
-        self.assertIn("ZZQB_high", hd.data.columns)
-        self.assertIn("ZZQB_low", hd.data.columns)
+    def _get_price_df(self, hd: HistoricalData):
+        price_cols = {old: old.split("_")[0] for old in hd.data.columns if old.endswith("close")}
+        price_df = hd.data.rename(columns = price_cols)[list(price_cols.values())]
+        return price_df
 
-class SignalCalculatorTest(unittest.TestCase):
+    def _share_price(self, stock, dt = None):
+        if dt == None:
+            return self.price_df.loc[self.current_dt,stock]
+        else:
+            return self.price_df.loc[dt,stock]
 
-    def test_constructor(self):
-        sd = SignalCalculator()
-        self.assertIsInstance(sd, SignalCalculator)
-        self.assertListEqual(list(sd.signals.keys()), ["aroon"])
+    def _sell_stock(self, stock, value = None, fraction = None):
+        if fraction != None:
+            share_price = self._share_price(stock)
+            num_shares = self.portfolio[stock]
+            self.cash += fraction * num_shares * share_price
+            self.portfolio[stock] -= fraction * num_shares
+            if fraction == 1:
+                self.portfolio.pop(stock)
+        # elif value != None:
 
-    def test_calculate_signals(self):
-        test_dir: Path = Path(__file__).parent.parent / "data" / "eod"
-        hd: HistoricalData = HistoricalData()
-        hd.load_data(test_dir)
-        signal_calc = SignalCalculator()
-        signal_df = signal_calc.calculate_signals(hd)
-        # Check that there is 1 column for each stock
-        self.assertEqual(len(signal_df.columns), 100)
-        # Check that all the original companies have a signal
-        self.assertListEqual(
-            sorted(signal_df.columns.tolist()),
-            sorted([f.stem+"_aroon" for f in test_dir.iterdir()])
-        )
-        # Check that the only signals in the dataframe are -1, 0, and 1.
-        for col in signal_df.columns:
-            for val in signal_df[col].unique():
-                if not np.isnan(val):
-                    self.assertIn(val, [-1.0,  0.0,  1.0])
+    def _buy_stock(self, stock, value):
+        share_price = self._share_price(stock)
+        shares_to_buy = value / share_price
+        self.cash -= value
+        if stock in self.portfolio:
+            self.portfolio[stock] += shares_to_buy
+        else:
+            self.portfolio[stock] = shares_to_buy
 
-    def test_calculate_aroon(self):
-        test_dir: Path = Path(__file__).parent.parent / "data" / "eod"
-        hd: HistoricalData = HistoricalData()
-        hd.load_data(test_dir)
-        signal_calc = SignalCalculator()
-        signal = signal_calc._calculate_aroon("AWU", hd.data)
+    def _portfolio_value(self):
+        asset_holdings = [
+            self._share_price(stock) * self.portfolio[stock] for stock in self.portfolio
+        ]
+        return sum(asset_holdings)
 
-        self.assertIsInstance(signal, pd.Series)
-        self.assertEqual(len(signal), 2516)
-        self.assertEqual(signal.name, "AWU_aroon")
+    def _calculate_return_series(self, series: pd.Series) -> pd.Series:
+        """
+        From Algorithmic Trading with Python
+        """
+        shifted_series = series.shift(1, axis=0)
+        return series / shifted_series - 1
+
+    def _get_years_passed(self, series: pd.Series) -> float:
+        """
+        From Algorithmic Trading with Python p.24
+        """
+
+        start_date = series.index[0]
+        end_date = series.index[-1]
+        return (end_date - start_date).days / 365.25
+
+    def _calculate_cagr(self, return_series: pd.Series) -> float:
+        """
+        From Algorithmic Trading with Python
+        """
+        value_factor = return_series.iloc[-1] / return_series.iloc[0]
+        years_passed = self._get_years_passed(return_series)
+        return (value_factor ** (1 / years_passed)) - 1
+
+    def _calculate_annualized_volatility(self, series: pd.Series) -> float:
+        """
+        From Algorithmic Trading with Python p.24
+        """
+        years_passed = self._get_years_passed(series)
+        entries_per_year = series.shape[0] / years_passed
+        return series.std() * np.sqrt(entries_per_year)
+
+    def _calculate_sharpe_ratio(self, price_series: pd.Series, benchmark_rate: float = 0) -> float:
+        """
+        From Algorithmic Trading with Python p.27
+        """
+        cagr = self._calculate_cagr(price_series)
+        return_series = self._calculate_return_series(price_series)
+        volatility = self._calculate_annualized_volatility(return_series)
+        return (cagr - benchmark_rate) / volatility
+
+    def run(self, signals_df: pd.DataFrame) -> None:
+        equity_curve = {}
+        dt_index = self.price_df.index
+        counter = 0
+        for dt in dt_index:
+            self.current_dt = dt
+            # print(dt)
+            signals = signals_df.loc[dt]
+            # print(signals)
+            stocks_to_sell: List[str] = [s for s in signals[signals == -1].index.tolist() if s in self.portfolio]
+            stocks_to_buy: List[str] = [s for s in signals[signals == 1].index.tolist() if s not in self.portfolio]
+            for stock in stocks_to_sell:
+                self._sell_stock(stock, fraction=1)
+            free_asset_slots = self.max_assets - len(self.portfolio.keys())
+            num_stocks_to_buy = min(len(stocks_to_buy), free_asset_slots)
+            if num_stocks_to_buy > 0:
+                available_cash_per_asset = 0.99 * self.cash / num_stocks_to_buy
+                if available_cash_per_asset > 10000:
+                    available_cash_per_asset = 10000
+                for stock in stocks_to_buy:
+                    if len(self.portfolio.keys()) <= self.max_assets:
+                        self._buy_stock(stock, available_cash_per_asset)
+            equity_curve[dt] = self.cash + self._portfolio_value()
+
+            # print(stocks_im_going_to_buy, stocks_im_going_to_sell)
+            # stocks_im_holding: List[str]  # This is determined by which stocks you bought previously
+            # counter +=1
+            # if counter > 23:
+            #     pass
+            # if counter > 28:
+            #     print("Stop date", dt)
+            #     print("Stop signals:", sorted(signals))
+            #     print("To sell:", stocks_to_sell)
+            #     print("To buy:", stocks_to_buy)
+            #     print(self.portfolio)
+            #     break
+        self.equity_curve = pd.Series(equity_curve)
+        self.equity_curve.index = pd.to_datetime(self.equity_curve.index)
+        self.sharpe_ratio = self._calculate_sharpe_ratio(self.equity_curve)
+
+if __name__ == "__main__":
+    eod_data_dir: Path = Path(__file__).parent.parent / "data" / "eod"
+
+    hd: HistoricalData = HistoricalData()
+    hd.load_data(eod_data_dir)
+
+    signal_calc: SignalCalculator = SignalCalculator()
+    signal_df: pd.DataFrame = signal_calc.calculate_signals(hd)
+
+    sim = TradingSimulator(hd)
+    sim.run(signal_df)
+    print("Sharpe Ratio", sim.sharpe_ratio)
+    sim.equity_curve.plot()
+
+    plt.show()
---
+++ src/aroon_simulator_tests.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+import unittest
+
+import numpy as np
+import pandas as pd
+
+from aroon_simulator import HistoricalData, SignalCalculator, TradingSimulator
+
+class HistoricalDataTestCase(unittest.TestCase):
+
+    def test_read_csv(self):
+        test_file: Path = Path(__file__).parent.parent / "data" / "eod" / "AWU.csv"
+        hd_df: pd.DataFrame = HistoricalData()._read_csv(test_file)
+        self.assertIn("AWU_high", hd_df.columns)
+        self.assertIn("AWU_low", hd_df.columns)
+        self.assertEqual(len(hd_df.columns), 2)
+
+    def test_load_data(self):
+        test_dir: Path = Path(__file__).parent.parent / "data" / "eod"
+        hd: HistoricalData = HistoricalData()
+        hd.load_data(test_dir)
+        self.assertEqual(len(hd.data.columns), 200)
+        self.assertIn("AWU_high", hd.data.columns)
+        self.assertIn("AWU_low", hd.data.columns)
+        self.assertIn("ZZQB_high", hd.data.columns)
+        self.assertIn("ZZQB_low", hd.data.columns)
+
+class SignalCalculatorTest(unittest.TestCase):
+
+    def test_constructor(self):
+        sd = SignalCalculator()
+        self.assertIsInstance(sd, SignalCalculator)
+        self.assertListEqual(list(sd.signals.keys()), ["aroon"])
+
+    def test_calculate_signals(self):
+        test_dir: Path = Path(__file__).parent.parent / "data" / "eod"
+        hd: HistoricalData = HistoricalData()
+        hd.load_data(test_dir)
+        signal_calc = SignalCalculator()
+        signal_df = signal_calc.calculate_signals(hd)
+        # Check that there is 1 column for each stock
+        self.assertEqual(len(signal_df.columns), 100)
+        # Check that all the original companies have a signal
+        self.assertListEqual(
+            sorted(signal_df.columns.tolist()),
+            sorted([f.stem for f in test_dir.iterdir()])
+        )
+        # Check that the only signals in the dataframe are -1, 0, and 1.
+        for col in signal_df.columns:
+            for val in signal_df[col].unique():
+                if not np.isnan(val):
+                    self.assertIn(val, [-1.0,  0.0,  1.0])
+
+    def test_calculate_aroon(self):
+        test_dir: Path = Path(__file__).parent.parent / "data" / "eod"
+        hd: HistoricalData = HistoricalData()
+        hd.load_data(test_dir)
+        signal_calc = SignalCalculator()
+        signal = signal_calc._calculate_aroon("AWU", hd.data)
+
+        self.assertIsInstance(signal, pd.Series)
+        self.assertEqual(len(signal), 2516)
+        self.assertEqual(signal.name, "AWU")
+
+
+class TradingSimulatorTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        test_dir: Path = Path(__file__).parent.parent / "data" / "eod"
+        self.hd: HistoricalData = HistoricalData()
+        self.hd.load_data(test_dir)
+        self.signal_calc = SignalCalculator()
+        self.signal_df = self.signal_calc.calculate_signals(self.hd)
+
+    def test_simulator_construction(self):
+        sim = TradingSimulator(self.hd)
+        self.assertIsInstance(sim, TradingSimulator)
+        self.assertEqual(sim.max_assets, 20)
+        self.assertEqual(sim.cash, 100000)
+
+    # @unittest.skip
+    def test_simulation_run(self):
+        sim = TradingSimulator(self.hd)
+        sim.run(self.signal_df)
+        print(sim.sharpe_ratio)
+
+    def test_get_share_price(self):
+        sim = TradingSimulator(self.hd)
+        self.assertEqual(191.7, sim._share_price("AWU", "2010-01-04"))
+        sim.current_dt = sim.price_df.index[1]
+        self.assertEqual(188.17, sim._share_price("AWU"))
+        self.assertEqual(42.95, sim._share_price("AXC"))
+
+    def test_sell_all_of_one_stock(self):
+        sim = TradingSimulator(self.hd)
+        sim.current_dt = sim.price_df.index[1]
+        sim.portfolio["AWU"] = 2
+        sim._sell_stock("AWU", fraction = 1)
+        self.assertNotIn("AWU", sim.portfolio)
+        self.assertEqual(sim.cash, 100376.34)
+
+    def test_sell_half_of_one_stock(self):
+        sim = TradingSimulator(self.hd)
+        sim.current_dt = sim.price_df.index[1]
+        sim.portfolio["AWU"] = 2
+        sim._sell_stock("AWU", fraction = 0.5)
+        self.assertEqual(sim.portfolio["AWU"], 1)
+        self.assertEqual(sim.cash, 100188.17)
+
+    def test_buy_stock(self):
+        sim = TradingSimulator(self.hd)
+        sim.current_dt = sim.price_df.index[1]
+        sim._buy_stock("AWU", 1000)
+        self.assertEqual(sim.portfolio["AWU"], 1000/188.17)
+        self.assertEqual(sim.cash, 99000)
+
+    def test_portfolio_balance_calculation(self):
+        sim = TradingSimulator(self.hd)
+        sim.current_dt = sim.price_df.index[1]
+        sim.portfolio = {
+            "AWU": 1,
+            "AXC": 2
+        }
+        portfolio_value = sim._portfolio_value()
+        self.assertEqual(188.17 + 2 * 42.95, portfolio_value)
