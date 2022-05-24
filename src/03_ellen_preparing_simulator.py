323974a2728f482c6cd7a2e8135bec1a51640b42
Elle <ellenyixinyu@gmail.com>
2022-05-24 19:03:08 -0400

    ran simulator code on cci generated signals

--- src/03_ellen_preparing_simulator.py
+++ src/03_ellen_preparing_simulator.py
@@ -3,18 +3,15 @@
 """
 Created on Thu May 19 19:35:15 2022
 
-Quick implementation of simulation + reporting of CAGR and Sharpe Ratio
-
-Pending: 
-    * Finishing implementing simulation
-    * Address questions I've noted below
-    * Visualize equity curve 
+Load all tickers and generate signals dataframe to prepare data for simulation. 
 
 @author: ellenyu
 
 """
 from load_ellen import * 
-from cci_ellen import * 
+from cci_ellen import *
+from signal_ellen import *
+from time import perf_counter
 
 #%%
 # Load the closing prices data 
@@ -29,13 +26,16 @@ df_prices = load_all_onecolumn()
 # df_ccimodified_pandas_df = pandas_df_ccimodified_rolling(df)
 # #print(df_cci)
 
-# ## Method 2  - use python_cci function 
+
+## Method 2  - use python_cci function 
+# print('Method 2 - python cci applied to columns with for loop:\n')
+# start = perf_counter()
 # # Create a list of cci for each ticker
 # list_cci=[]
-# list_cci.append(df.index)
- 
-# for column in df.columns: 
-#     cci = python_ccimodified_loop(df[column].tolist(), window=3) # Because python_cci_loop processes one stock at a time
+# list_cci.append(df_prices.index)
+
+# for column in df_prices.columns: 
+#     cci = python_ccimodified_loop(df_prices[column].tolist()) # Because python_cci_loop processes one stock at a time
 #     list_cci.append(cci)
 
 # # print('verifying tranpose:', list_cci[1][:20], '\n')
@@ -45,44 +45,40 @@ df_prices = load_all_onecolumn()
 # df_ccimodified_python = pd.DataFrame(list_cci).T
 # # print('verifying tranpose:', df_cci_python.iloc[:20,1], '\n')
 
-# df_ccimodified_python.columns = df.reset_index().columns
+# df_ccimodified_python.columns = df_prices.reset_index().columns
 # # print('verifying column names:\n', df_cci_python.iloc[:, :3], '\n')
 # # print('shape:', df_cci_python.shape, '\n')
 
 # df_ccimodified_python = df_ccimodified_python.set_index('date')
-# # print('shape:', df_cci_python.shape, '\n')
-# # print(df_cci_python)
+# stop = perf_counter()
+# print('elapsed time:', stop-start, 'seconds\n')
+# # print('shape:', df_ccimodified_python.shape, '\n')
+# print(df_ccimodified_python)
+
 
 ## Method 3 - create a load_all_onecolumn_with_cci function 
-df_ccimodified = load_all_onecolumn_with_ccimodified()
+# print('Method 3 - python cci applied to columns with custom load function:\n')
+# start = perf_counter()
+# df_ccimodified = load_all_onecolumn_with_ccimodified()
+# stop = perf_counter()
+# print('elapsed time:', stop-start, 'seconds\n')
+# print(df_ccimodified)
+
+
+## Method 4 - use apply! [Chris suggestion]
+print('Method 4 - python cci applied to columns with apply:\n')
+start = perf_counter()
+df_ccimodified = df_prices.apply(lambda col: python_ccimodified_loop(col.tolist()), axis=0)
+stop = perf_counter()
+print('elapsed time:', stop-start, 'seconds\n')
 print(df_ccimodified)
+
+
+## Observation 
+# .apply method is fastest by 0.5 seconds. Can use .apply moving forward 
 
 #%%
 # Load the generated signals data
-
-def cci_signals_generator(series: pd.Series) -> pd.Series:
-    
-    '''
-    cci is like a z-score which means 1 contains roughly 68% of the data, 2 contains 
-    roughly 95% of the data, 3 contains roughly 97% of the data, and 4 contains roughly 
-    99% of the data. 
-    
-    If cci > 3 then buy, if cci < -2 sell [Chris suggestion] # ??? Double check that this is not the other way around
-    '''
-    # Generate buy and sell signals
-    # print('series:\n', series, '\n')
-    # print('where series > 3:\n', series[series>3], '\n')
-    # print('where series <-2:\n', series[series<-2], '\n')
-    #Sell when price is greater than 3 z-scores and buy when price is less than -2 z-scores
-    signals = -1 * (series > 3) \
-                + 1 * (series <-2) #??? Not sure why this line of code works, but I've checked across 3 tickers that this works 
-    # print(signals.value_counts())
-    # print('signals:', signals, '\n')
-    
-    #assert type(signals) == pd.Series, "Output array is not same type as input array"
-    assert len(signals) == len(series), "Output array is not same length as input array"
-    
-    return signals
 
 ## Method 1 - Iterate over columns, and apply cci_signals_generator function
 print('method 1 - iterate over columns:\n')
@@ -90,7 +86,7 @@ print('method 1 - iterate over columns:\n')
 df_cci_signals = pd.DataFrame()
  
 for column in df_ccimodified.columns: 
-    cci_signals = cci_signals_generator(df_ccimodified[column]) # Because I wrote cci_signals_generator to process one stock at a time
+    cci_signals = cci_signals_generator_reversal(df_ccimodified[column]) # Because I wrote cci_signals_generator to process one stock at a time
     df_cci_signals = pd.concat([df_cci_signals, cci_signals], axis=1)
 
 #print(df_cci_signals)
@@ -109,7 +105,7 @@ print(df_tab.iloc[:, :3], '\n')
 ## Method 2 - Apply cci_signals_generator function to dataframe 
 print('method 1 - apply to the dataframe:\n')
 
-df_cci_signals = cci_signals_generator(df_ccimodified)
+df_cci_signals = cci_signals_generator_reversal(df_ccimodified)
 
 #print(df_cci_signals)
 
@@ -127,59 +123,59 @@ print(df_tab.iloc[:, :3], '\n')
 # Applying a function made for pd.Series on pd.DataFrame works. Use but keep tabs on it 
 
 #%% 
-# Run through a simulator [largely Chris' implementaion]
-prices_df = df_prices
-signal_df = df_cci_signals
+# # Run through a simulator [largely Chris' implementaion]
+# prices_df = df_prices
+# signal_df = df_cci_signals
 
-assert prices_df.index.equals(signal_df.index), "Indices do not equal"
+# assert prices_df.index.equals(signal_df.index), "Indices do not equal"
 
-max_assets = 20
-dt_index = prices_df.index
-cash = starting_cash = 100_000 #??? Underscores are acceptable in int?
-portfolio: List[str] = list()
-equity_curve = dict()
-
-
-# Pretend you are walking through time trading over the course of ten years
-for date in dt_index:
-
-	signals = signal_df.loc[date] 
-
-	stocks_im_going_to_buy: List[str] = signals[signals == 1].index.tolist() #??? Stock names are retained here?
-	stocks_im_going_to_sell: List[str] = signals[signals == -1].index.tolist()
-	stocks_im_holding: List[str] # This is determined by which stocks you bought previously [Pending, I think]
-
-	# Mess with your cash and portfolio to sell stocks
-	for stock in stocks_im_going_to_sell:
-		cash += prices_df.loc[date, stock] * #number of shares of stock [Pending: where are we holding number of shares?]
-		portfolio.pop(stock)
-		stocks_im_holding.pop(stock)
-
-	# What do I do with too many buy signals?
-	# Mess with your cash and portfolio to buy stocks
-	for stock in stocks_im_going_to_buy:
-		# This is the "compound your gains and losses" approach to cash management
-		# Also called the "fixed number of slots" approach
-		cash_to_spend = cash / (max_assets - len(stocks_im_holding)) #[Pending: will run into DivisionByZeroError]
-
-		# Straight from the book ... See page 68
-		# self.free_position_slots = self.max_active_position - self.active_positions_count
-		# cash_to_spend = self.cash / self.free_position_slots
-
-		cash -= cash_to_spend
-		portfolio.append(stock)
-		stocks_im_holding.append(stock) 
-        #[Pending: are we doing to track number of shares of stock? ]
-
-	equity_curve[date] = cash + # sum of my portfolio value [Pending]
+# max_assets = 20
+# dt_index = prices_df.index
+# cash = starting_cash = 100_000 #??? Underscores are acceptable in int?
+# portfolio: List[str] = list()
+# equity_curve = dict()
 
 
-# Plot the equity curve [Pending]
+# # Pretend you are walking through time trading over the course of ten years
+# for date in dt_index:
 
-# Report the CAGR [Pending]
-calculate_cagr(equity_curve)
+# 	signals = signal_df.loc[date] 
 
-# Measure the sharpe ratio [Pending]
-calculate_sharpe_ratio(equity_curve)
+# 	stocks_im_going_to_buy: List[str] = signals[signals == 1].index.tolist() #??? Stock names are retained here?
+# 	stocks_im_going_to_sell: List[str] = signals[signals == -1].index.tolist()
+# 	stocks_im_holding: List[str] # This is determined by which stocks you bought previously [Pending, I think]
 
-# You're done.
+# 	# Mess with your cash and portfolio to sell stocks
+# 	for stock in stocks_im_going_to_sell:
+# 		cash += prices_df.loc[date, stock] * #number of shares of stock [Pending: where are we holding number of shares?]
+# 		portfolio.pop(stock)
+# 		stocks_im_holding.pop(stock)
+
+# 	# What do I do with too many buy signals?
+# 	# Mess with your cash and portfolio to buy stocks
+# 	for stock in stocks_im_going_to_buy:
+# 		# This is the "compound your gains and losses" approach to cash management
+# 		# Also called the "fixed number of slots" approach
+# 		cash_to_spend = cash / (max_assets - len(stocks_im_holding)) #[Pending: will run into DivisionByZeroError]
+
+# 		# Straight from the book ... See page 68
+# 		# self.free_position_slots = self.max_active_position - self.active_positions_count
+# 		# cash_to_spend = self.cash / self.free_position_slots
+
+# 		cash -= cash_to_spend
+# 		portfolio.append(stock)
+# 		stocks_im_holding.append(stock) 
+#         #[Pending: are we doing to track number of shares of stock? ]
+
+# 	equity_curve[date] = cash + # sum of my portfolio value [Pending]
+
+
+# # Plot the equity curve [Pending]
+
+# # Report the CAGR [Pending]
+# calculate_cagr(equity_curve)
+
+# # Measure the sharpe ratio [Pending]
+# calculate_sharpe_ratio(equity_curve)
+
+# # You're done.
---
+++ src/04_ellen_simulate.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 24 08:54:17 2022
+
+Explored the performance of cci via two simulators. 
+
+Pending: 
+    * double check if quick simulator is incorrect because the results 
+    (equiy curve, cagr, sharpe ratio) are horrible.  
+
+@author: ellenyu
+
+"""
+import pandas as pd
+import numpy as np
+from load_ellen import * 
+from cci_ellen import * 
+from signal_ellen import * 
+from vqti.performance import calculate_cagr, calculate_sharpe_ratio
+from pypm import metrics, signals, data_io, simulation
+
+# Load prices df 
+price_df = load_all_onecolumn()
+#print(price_df, '\n')
+
+# Calculate technical indicator, cci (window defaults to 20)
+indicator_df = price_df.apply(lambda col: python_ccimodified_loop(col.tolist()), axis=0)
+#print(indicator_df, '\n')
+
+# # Calculate signals df (reversal strategy)
+# signal_df = cci_signals_generator_reversal(indicator_df)
+# #print(signal_df, '\n')
+
+# Calculate signals df (momentum strategy)
+signal_df = cci_signals_generator_momentum(indicator_df, upper_band = 2, lower_band = -1)
+#print(signal_df, '\n')
+
+# Check signals df contains buy, sell, and hold signals 
+melt_df = signal_df.melt(var_name='columns', value_name='index')
+tab_df = pd.crosstab(index=melt_df['index'], columns=melt_df['columns'])
+#print(tab_df, '\n')
+
+#%%
+# Method - Function
+def quick_simulator (price_df: pd.DataFrame, signal_df: pd.DataFrame, cash: int = 100_000, num_stocks: int = 20):
+    
+    # Double check signal df which is created off price df has the same index as price df 
+    assert price_df.index.equals(signal_df.index), "Indices do not equal"
+
+    # Initiate dictionaries 
+    shares_dict: Dict [stock: str, num_share: int] = {}
+    stocks_dict: Dict [date, stocks: List[str]] = {}
+    cash_dict: Dict [date, cash: int] = {}
+    portfolio_dict: Dict [date, value: int ]= {}
+    equity_dict: Dict [date, value: int ]= {}
+    
+    # Fixed number of stocks                    
+    cash_to_spend = cash / num_stocks
+    
+    # Start simulator code 
+    for date in price_df.index:
+        #print(type(date)) #<class 'pandas._libs.tslibs.timestamps.Timestamp'>
+        for stock in price_df.columns:
+            signal = signal_df.loc[date, stock]
+            #print(type(signal)) #<class 'numpy.longlong'>
+            price = price_df.loc[date, stock] 
+            # print(type(price)) #<class 'numpy.float64'> 
+            if signal == 1 and cash > cash_to_spend: 
+                #Buy 
+                cash -= cash_to_spend
+                shares = cash_to_spend / price
+                shares_dict[stock] = shares 
+            elif signal == -1 and stock in list(shares_dict.keys()):
+                #Sell 
+                stock_value = price * shares_dict[stock]
+                cash += stock_value
+                shares_dict.pop(stock)
+            else: 
+                #Do nothing 
+                pass
+        # Keep a few records 
+        cash_dict [date] = cash
+        stocks_dict[date] = list(shares_dict.keys())
+        value = 0 
+        for stock in stocks_dict[date]:
+            value += shares_dict[stock] * price_df.loc[date, stock]
+        portfolio_dict [date] = value
+        equity_dict [date] = cash_dict[date] + portfolio_dict[date]
+    
+    return cash_dict, portfolio_dict, equity_dict
+
+#%%
+cash_dict, portfolio_dict, equity_dict = quick_simulator(price_df = price_df, signal_df = signal_df)
+
+plt.plot(cash_dict.keys(), cash_dict.values())
+plt.title('Cash over Time')
+plt.show()
+plt.plot(portfolio_dict.keys(), portfolio_dict.values())
+plt.title('Portfolio over Time')
+plt.show()
+plt.plot(equity_dict.keys(), equity_dict.values())
+plt.title('Equity over Time')
+plt.show()        
+
+#%%
+cagr = calculate_cagr(pd.Series(equity_dict))
+print(cagr)
+
+sharpe = calculate_sharpe_ratio(pd.Series(equity_dict))
+print(sharpe)
+
+#%%     
+# Method - Class 
+signal = signal_df
+prices = price_df
+
+preference = pd.DataFrame(0, index=prices.index, columns=prices.columns)
+
+
+assert signal.index.equals(preference.index)
+assert prices.columns.equals(signal.columns)
+assert signal.columns.equals(preference.columns)
+
+# Run the simulator
+simulator = simulation.SimpleSimulator(
+    initial_cash=100_000,
+    max_active_positions=20,
+    percent_slippage=0.0005,
+    trade_fee=1,
+)
+simulator.simulate(prices, signal, preference)
+
+
+# Print results
+simulator.portfolio_history.print_position_summaries()
+simulator.print_initial_parameters()
+simulator.portfolio_history.print_summary()
+simulator.portfolio_history.plot()
+simulator.portfolio_history.plot_benchmark_comparison()
